8de645941cb55bf596eab0471c44be605b797046
Sujith Christopher <50609700+SujithChristopher@users.noreply.github.com>
2022-01-29 10:34:51 +0530

    asdfff

--- recorder_cam_imu.py
+++ recorder_cam_imu.py
@@ -71,17 +71,17 @@ from bleak import discover
 import csv
 
 
-
-
 # These values have been randomly generated - they must match between the Central and Peripheral devices
 # Any changes you make here must be suitably made in the Arduino program as well
 IMU_UUID = '13012F01-F8C3-4F4A-A8F4-15CD926DA146'
 IMU_UUID1 = '13012F05-F8C3-4F4A-A8F4-15CD926DA146'
 
+imu = 0
+
 # Class for handling BLE communication with a Nano board for receiving IMU data.
 class NanoIMUBLEClient(object):
     
-    def __init__(self, uuid:str) -> None:
+    def __init__(self, uuid:str, imu:int) -> None:
         super().__init__()
         self._client = None
         self._device = None
@@ -93,6 +93,7 @@ class NanoIMUBLEClient(object):
                       "gx": 0.0, "gy": 0.0, "gz": 0.0} 
         self.newdata = False
         self.printdata = True
+        self._imu = imu
         
         self.counter = 0
         self._csv_file = open('outputdata.csv', 'w')
@@ -129,21 +130,22 @@ class NanoIMUBLEClient(object):
         print('Looking for Peripheral Device...')
         devices = await discover()
         for d in devices:
-            
-            if 'BLE LEFT'in d.name:
-                self._found = True
-                self._device = d
-                sys.stdout.write(f'Found Peripheral Device {self._device.address}. ')
-                break
-            
-            if 'BLE RIGHT'in d.name:
-                self._found = True
-                self._device = d
-                sys.stdout.write(f'Found Peripheral Device {self._device.address}. ')
-                break
+            if self._imu == 1:
+                if 'BLE LEFT'in d.name:
+                    self._found = True
+                    self._device = d
+                    sys.stdout.write(f'Found Peripheral Device {self._device.address}. ')
+                    break
+            if self._imu == 2:
+                if 'BLE RIGHT'in d.name:
+                    self._found = True
+                    self._device = d
+                    sys.stdout.write(f'Found Peripheral Device {self._device.address}. ')
+                    break
         
         # Connect and do stuff.
         async with BleakClient(d.address) as self._client:
+            
             sys.stdout.write(f'Connected.\n')
             self._connected = True
             # Start getting data.
@@ -158,6 +160,7 @@ class NanoIMUBLEClient(object):
                     await asyncio.sleep(0)
     
     async def disconnect(self) -> None:
+        
         if self._connected:
             # Stop notification first.
             self._client.stop_notify()
@@ -166,17 +169,20 @@ class NanoIMUBLEClient(object):
             self._running = False
     
     async def start(self) -> None:
+        
         if self._connected:
             # Start notification
             await self._client.start_notify(self._uuid, self.newdata_hndlr)
             self._running = True
     
     async def stop(self) -> None:
+        
         if self._running:
             # Stop notification
             await self._client.stop_notify(self._uuid)
         
     def newdata_hndlr(self, sender, data):
+        
         self._data['ax'] = struct.unpack('<f', bytes(data[0:4]))[-1]
         self._data['ay'] = struct.unpack('<f', bytes(data[4:8]))[-1]
         self._data['az'] = struct.unpack('<f', bytes(data[8:12]))[-1]
@@ -186,7 +192,8 @@ class NanoIMUBLEClient(object):
         self.newdata = True
     
     def print_newdata(self) -> None:
-        _str = ("\r Accl: " +
+        
+        _str = ("\r Accl: " + str(self._imu)+
                 f"{self.data['ax']:+1.3f}, " + 
                 f"{self.data['ay']:+1.3f}, " + 
                 f"{self.data['az']:+1.3f} | " +
@@ -201,23 +208,37 @@ class NanoIMUBLEClient(object):
         sys.stdout.write(_str)
         sys.stdout.flush()
         
-        
-
 
 async def run_imu():
-    # Create a new IMU client.
             
-    imu_client_1 = NanoIMUBLEClient(IMU_UUID)
-    imu_client_2 = NanoIMUBLEClient(IMU_UUID1)
+    imu_client_1 = NanoIMUBLEClient(IMU_UUID, 1)
+    # imu_client_2 = NanoIMUBLEClient(IMU_UUID1)
     await imu_client_1.connect()
-    await imu_client_2.connect()
+    # await imu_client_2.connect()
 
 #intermediate callback to execure async funciton    
 def between_callback():
+    
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
     loop.run_until_complete(run_imu())
+    loop.close()
+    
+async def run_imu_2():
+    # Create a new IMU client.
+            
+    # imu_client_1 = NanoIMUBLEClient(IMU_UUID)
+    imu_client_2 = NanoIMUBLEClient(IMU_UUID1, 2)
+    # await imu_client_1.connect()
+    await imu_client_2.connect()
+
+#intermediate callback to execure async funciton    
+def between_callback2():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    loop.run_until_complete(run_imu_2())
     loop.close()
     
 # running camera application
@@ -231,17 +252,17 @@ if __name__ == "__main__":
         
     t1 = threading.Thread(target=between_callback)
     t2 = threading.Thread(target=run_camera)
+    t3 = threading.Thread(target=between_callback2)
   
     # starting thread 1
     t1.start()
     # starting thread 2
     t2.start()
+    t3.start()
   
     # wait until thread 1 is completely executed
     t1.join()
     # wait until thread 2 is completely executed
     t2.join()
-    
-    
-    
+    t3.join()
     
